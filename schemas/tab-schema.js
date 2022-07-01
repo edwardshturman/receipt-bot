7d70c82c621a5bd8fa4df1d91359fe933b427fed
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2022-06-30 18:14:51 -0700

    Create `tabSchema`

---
+++ schemas/tab-schema.js
@@ -0,0 +1,15 @@
+const tabCommand = require('../commands/tab');
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tabSchema = new Schema({
+    name: String,
+    creditor: String,
+    roleId: String,
+    members: Array
+}, { collection: 'tabs' });
+
+const Tab = mongoose.model('tab', tabSchema);
+
+module.exports = Tab;
